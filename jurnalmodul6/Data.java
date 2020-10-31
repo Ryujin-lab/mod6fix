1603ea1e88e2fedea45e13a9f9ccc562d0f66669
Udin <aliedyne222@gmail.com>
2020-10-31 10:38:07 +0800

    oke

---
+++ jurnalmodul6/Data.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnalmodul6;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ryuujin
+ */
+public class Data {
+    public static ArrayList<Fruit> fruit = new ArrayList<Fruit> ();
+}
---
+++ jurnalmodul6/Fruit.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnalmodul6;
+
+/**
+ *
+ * @author ryuujin
+ */
+public class Fruit {
+    String name;
+    int count;
+    Fruit(String name, int count){
+        this.name = name;
+        this.count = count;
+    }
+}
